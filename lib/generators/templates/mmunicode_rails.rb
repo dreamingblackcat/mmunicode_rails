31af2d5b3946a03226c9bfba05071c0debb7bc5b
dreamingblackcat <dreamingblackcat10@gmail.com>
2015-02-11 11:09:32 +0630

    remove unnecessary require from the template file

--- lib/generators/templates/mmunicode_rails.rb
+++ lib/generators/templates/mmunicode_rails.rb
@@ -1,3 +1,2 @@
-require 'mmunicode_rails'
-puts "Inserting middleware mmunicdoe"
+puts "Inserting middleware mmunicode"
 Rails.application.config.middleware.insert_before(ActionDispatch::ParamsParser,MmunicodeRails::RackMmunicode)
