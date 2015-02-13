cc758010f3082b1df48c311d8e980e6df7bfb476
Ye Lin Aung <me@yelinaung.com>
2015-02-13 15:57:39 +0630

    no more offenses for generator

--- lib/generators/mmunicode_rails/initializer_generator.rb
+++ lib/generators/mmunicode_rails/initializer_generator.rb
@@ -1,15 +1,14 @@
 module MmunicodeRails
   module Generators
+    # Generator
     class InitializerGenerator < ::Rails::Generators::Base
-
-      source_root File.expand_path("../../templates", __FILE__)
+      source_root File.expand_path('../../templates', __FILE__)
 
       desc 'Creates MmunicodeRails initializer.'
 
       def copy_initializer
         copy_file 'mmunicode_rails.rb', 'config/initializers/mmunicode_rails.rb'
       end
-
     end
   end
 end
